9cb68160d6da5db4e4b5011d839c4fa025fa39bb
Ugbaad <ugbaad0012@gmail.com>
2023-03-15 09:59:54 +0100

    nyt

--- src/main/java/com/example/githubtest/HomeController.java
+++ src/main/java/com/example/githubtest/HomeController.java
@@ -1,9 +1,10 @@
+package com.example.githubtest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
     @GetMapping("/")
     public String index () {
         return "Hello world!";
